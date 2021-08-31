a8811a343d7b4da49fde45d0e49b80c4ebbe2034
Palash <tanejapalash@gmail.com>
2021-08-31 16:58:31 -0500

    refactor key

--- SecureCudaArray.cuh
+++ SecureCudaArray.cuh
@@ -8,6 +8,7 @@
 #include "AES.h"
 
 #define AES_BLOCK_SIZE 16
+#define AES_BITS 128
 
 template <class T>
 class SecureCudaArray
@@ -15,18 +16,20 @@ class SecureCudaArray
 public:
     explicit SecureCudaArray()
         : start_(0),
-          end_(0)
+          end_(0),
+          key { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f }
     {
-        aesCPU = new AES(128);
+        aesCPU = new AES(AES_BITS);
         encryptedSize = 0;
     }
 
     explicit SecureCudaArray(size_t size)
     {
-        size_t padding = size % 16;
+        size_t padding = size % AES_BLOCK_SIZE;
         size += padding;
         allocate(size);
-        aesCPU = new AES(128);
+
+        aesCPU = new AES(AES_BITS);
         encryptedSize = size;
     }
 
@@ -50,7 +53,6 @@ public:
     {
         if (encryptedSize == 0) return start_;
 
-        unsigned char key[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f }; 
         decryptdemo(key, (uint8_t*) start_, encryptedSize);
         encryptedSize = 0;
         return start_;
@@ -60,7 +62,6 @@ public:
     {
         if (encryptedSize == 0) return start_;
 
-        unsigned char key[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f }; 
         decryptdemo(key, (uint8_t*) start_, encryptedSize * sizeof(T));
         encryptedSize = 0;
         return start_;
@@ -69,7 +70,6 @@ public:
     void set(const T* src, size_t size)
     {
         unsigned int outLen;
-        unsigned char key[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f }; 
         auto out = aesCPU->EncryptECB((unsigned char*) src, size * sizeof(T), key, outLen);
         cudaError_t result = cudaMemcpy(start_, out, size * sizeof(T), cudaMemcpyHostToDevice);
 
@@ -115,6 +115,7 @@ private:
     T* start_;
     T* end_;
     // in a real system, there would be a mechanism for the GPU and CPU to decide on a key at start
+    unsigned char key[16];
     AES* aesCPU;
     uint32_t encryptedSize;
 };
